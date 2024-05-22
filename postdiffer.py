d7edf212ca63f0ce93857a4dd733a6e296e29574
Arthur <59236083+tilmana@users.noreply.github.com>
2024-05-22 14:45:09 -0400

    Update postdiffer.py
    Moved from arrays to dictionaries as this is a better solution

--- postdiffer.py
+++ postdiffer.py
@@ -1,10 +1,8 @@
 oldRequest = open("oldrequest.txt", "r")
 newRequest = open("newrequest.txt", "r")
 
-oldParams = []
-oldValues = []
-newParams = []
-newValues = []
+oldData = {}
+newData = {}
 
 oldRequestData = oldRequest.readlines()[0].split("&")
 newRequestData = newRequest.readlines()[0].split("&")
@@ -12,25 +10,18 @@ newRequestData = newRequest.readlines()[0].split("&")
 for value in oldRequestData:
     value = value.split("=")
     if len(value) == 2:
-        oldParams.append(value[0])
-        oldValues.append(value[1])
+        oldData[value[0]] = value[1]
 
 for value in newRequestData:
     value = value.split("=")
     if len(value) == 2:
-        newParams.append(value[0])
-        newValues.append(value[1])
+        newData[value[0]] = value[1]
 
-oldParams = sorted(oldParams)
-oldValues = sorted(oldValues)
-newParams = sorted(newParams)
-newValues = sorted(newValues)
+for key in oldData:
+    if key in newData:
+        if oldData[key] != newData[key]:
+            print(f"Difference in {key}!")
+            print(f"{oldData[key]} / {newData[key]}")
 
-for i in range(min(len(oldParams), len(newParams))):
-    diffCount = abs(len(oldParams) - len(newParams))
-    print(f"[!] Skipped {diffCount} parameters due to difference in count between request bodies!")
-    if oldValues[i] == newValues[i]:
-        pass
-    else:
-        print(f"Diff in {oldParams[i]}")
-        print(oldValues[i] + " / " + newValues[i])
+diffCount = abs(len(oldData) - len(newData))
+print(f"[!] Skipped {diffCount} parameters due to difference in count between request bodies!")
